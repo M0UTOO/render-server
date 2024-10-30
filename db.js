d1333c95bc090ec02ec74ce647a0db7246190341
Nino <ninoplane@gmail.com>
2024-10-30 14:57:45 +0100

    added db connecdtion

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
@@ -14,7 +14,12 @@ const sequelize = new Sequelize(
     define: {
       createdAt: 'added',
       updatedAt: 'updated',
-    }
+    },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
   },
 )
 
